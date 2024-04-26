00a8b3ff30dc83b7df41d23226f672cd14648baa
arungupta0337 <149996746+arungupta0337@users.noreply.github.com>
2024-04-26 13:01:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+let savedToken = '';
 
 app.get('/', (req, res) => {
   res.send('Successful response.');
 });
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.post('/saveToken', (req, res) => {
+  const { token } = req.body;
+  if (token) {
+    savedToken = token;
+    res.status(200).json({ data: "Saved the token" });
+  } else {
+    res.status(400).json({ error: 'Token not provided' });
+  }
+});
+
+app.get('/getToken', (req, res) => {
+  if (savedToken) {
+    res.send(savedToken);
+  } else {
+    res.send('');
+  }
+});
+
+app.listen(5000, () => console.log('Example app is listening on port 5000.'));
